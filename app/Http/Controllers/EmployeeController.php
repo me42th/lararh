71f7e3a64c2b40fd8ab52ced03e58b160c17fb06
me42th <david.meth@hotmail.com>
2024-03-12 12:59:39 -0300

    [1] swagger

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -15,17 +15,93 @@ class EmployeeController extends Controller
 
     /**
      * @OA\Get(
-     *     path="/api/employee",
-     *     @OA\Response(response="200", description="An example endpoint")
+     *     path="/employee",
+     *     tags={"employee"},
+     *     summary="Finds employees by filter",
+     *     description="Filter values can be provided",
+     *     @OA\Parameter(
+     *         name="gender",
+     *         in="query",
+     *         description="Gender values for filter",
+     *         required=false,
+     *         explode=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             enum={"M", "F"},
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="first_name",
+     *         in="query",
+     *         description="First name for filter",
+     *         required=false,
+     *         explode=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="last_name",
+     *         in="query",
+     *         description="Last name for filter",
+     *         required=false,
+     *         explode=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="hire_date",
+     *         in="query",
+     *         description="Hire date for filter",
+     *         required=false,
+     *         explode=true,
+     *         @OA\Schema(
+     *             type="date"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="birth_date",
+     *         in="query",
+     *         description="Birth date for filter",
+     *         required=false,
+     *         explode=true,
+     *         @OA\Schema(
+     *             type="date"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/employee")
+     *         )
+     *     )
      * )
      */
     public function index()
+
     {
         //
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Add a new employee
+     *
+     * @OA\Post(
+     *     path="/employee",
+     *     tags={"employee"},
+     *     @OA\Response(
+     *         response=422,
+     *         description="Invalid input"
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Created"
+     *     ),
+     *     @OA\RequestBody(ref="#/components/requestBodies/employee")
+     * )
      */
     public function store(Request $request)
     {
@@ -33,7 +109,37 @@ class EmployeeController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * @OA\Get(
+     *     path="/employee/{employeeId}",
+     *     tags={"employee"},
+     *     summary="Find employee by ID",
+     *     description="Returns a single employee",
+     *     @OA\Parameter(
+     *         name="employeeId",
+     *         in="path",
+     *         description="ID of employee to return",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent(ref="#/components/schemas/employee")
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid ID supplier"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Employee not found"
+     *     )
+     * )
+     *
+     * @param int $id
      */
     public function show(string $id)
     {
---
+++ app/Models/Employee.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @OA\Schema(
+ *  schema="employee",
+ *  type="object",
+ *  description="Schema of Model User",
+ *  @OA\Property(
+ *      property="emp_no",
+ *      type="integer",
+ *      description="emp_no",
+ *      title="emp_no",
+ *      example="42"
+ *  ),
+ *  @OA\Property(
+ *      property="first_name",
+ *      type="string",
+ *      description="First name of employee",
+ *      example="John"
+ *  ),
+ *  @OA\Property(
+ *      property="last_name",
+ *      type="string",
+ *      description="Last name of employee",
+ *      example="Doe"
+ *  ),
+ *  @OA\Property(
+ *      property="birth_date",
+ *      type="date",
+ *      description="Birth date of employee",
+ *      example="1990-12-13"
+ *  ),
+ *  @OA\Property(
+ *      property="gender",
+ *      type="string",
+ *      description="Gender of employee",
+ *      enum={"M", "F"},
+ *  ),
+ *  @OA\Property(
+ *      property="hire_date",
+ *      type="date",
+ *      description="Hire date of employee",
+ *      example="1990-12-13"
+ *  ),
+ * )
+ * @OA\RequestBody(
+ *     request="employee",
+ *     description="Employee object",
+ *     required=true,
+ *     @OA\JsonContent(ref="#/components/schemas/employee")
+ * )
+ */
+
+
+class Employee extends Model
+{
+    use HasFactory;
+
+}
